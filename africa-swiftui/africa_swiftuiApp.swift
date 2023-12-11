52f33e8bfa75c5eaabaecbb1ceef600cd2eb849f
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-11 09:25:10 +0530

    initial setup

---
+++ africa-swiftui/africa_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  africa_swiftuiApp.swift
+//  africa-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct africa_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
