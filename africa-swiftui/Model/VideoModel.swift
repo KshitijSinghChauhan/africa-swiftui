f98b3671a2c211f6d8c553dc9ee3642545a69cd3
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-11 18:54:52 +0530

    feature: added VideoModel and Video List Item file

---
+++ africa-swiftui/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  africa-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 11/12/23.
+//
+
+import SwiftUI
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
---
+++ africa-swiftui/View/VideoListItem.swift
@@ -0,0 +1,54 @@
+//
+//  VideoListItem.swift
+//  africa-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 11/12/23.
+//
+
+import SwiftUI
+
+struct VideoListItem: View {
+    // MARK: - PROPERTIES
+    
+    let video: Video
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(spacing: 10) {
+            ZStack {
+                Image("video-\(video.id)")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            } //: ZSTACK
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            } //: VSTACK
+        } //: HSTACK
+    }
+}
+
+struct VideoListItem_Previews: PreviewProvider {
+    static let video: [Video] = Bundle.main.decode("videos.json")
+    static var previews: some View {
+        VideoListItem(video: video[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
